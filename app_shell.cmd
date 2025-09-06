e688cc0f2c4520c6e1e1eb853683f017a7fca200
michalr <homeend@wp.pl>
2025-09-07 00:18:10 +0200

    scripts for contenorisation

--- app_shell.cmd
+++ app_shell.cmd
@@ -1,2 +1,2 @@
 call app_build.cmd
-docker compose -f compose.yml run --build --remove-orphans -P feeds-site-builder /bin/bash
+docker compose -f compose.yml run --build --remove-orphans --rm -P feeds-site-builder /bin/bash
